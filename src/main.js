0b6607d25dd325289184d8c59ae3d6f6d7bb405a
Daniel Kelly <me@danielkelly.io>
2024-01-17 15:20:27 -0600

    return custom devtools for now

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // You do not need to change anything in this file
 import { createApp } from "vue";
+import "@vueschool/browser-devtools";
 import App from "./App.vue";
 import "@/assets/main.css";
 
